271b8db7d1663914023ae739eb031584beb42662
AnshPrakash <anshprakash1997@gmail.com>
2019-08-17 20:27:54 +0530

    Almost complete

--- Assignments/A0/A0.ml
+++ Assignments/A0/A0.ml
@@ -44,8 +44,8 @@ let rec letters p s= match p with
 ;;
 
 let rho = Hashtbl.create 1234;; (*HashTable*)
-Hashtbl.add rho "h" true;;
-Hashtbl.add rho "Aux" true;;
+Hashtbl.add rho "A" false;;
+Hashtbl.add rho "B" true;;
 
 let rec truth p rho = match p with
 					| T -> true
@@ -78,16 +78,34 @@ let rec nnf p = match p with
 ;;
 
 
+let rec subAnd p  = match p with
+				| T -> false
+				| F -> false
+				| Not(q) -> false
+				| And(q1,q2) -> true
+				| Or(q1,q2) -> (subAnd q1) || (subAnd q2)
+				| L(st) -> false
+;;
+
+let rec subOr p  = match p with
+				| T -> false
+				| F -> false
+				| Not(q) -> false
+				| Or(q1,q2) -> true
+				| And(q1,q2) -> (subOr q1) || (subOr q2)
+				| L(st) -> false
+;;
+
 let rec cnff p = match p with
 				| T -> T
 				| F -> F
 				| Not(q) -> p
 				| And(q1,q2) -> And(cnff q1, cnff q2)
-				| Or(And(q1,w),q2) -> cnff (nnf (And(Impl(L("Aux"),And(q1,w)),Impl(Not(L("Aux")),q2))))  (* convert it to nnf again*)
-				| Or(q1,And(q2,w)) -> cnff (nnf (And(Impl(L("Aux"),q1),Impl(Not(L("Aux")),And(q2,w)))))
-				| Or(Or(q1,w),q2) -> cnff (Or(cnff (Or(q1,w)) ,q2))
-				| Or(q1,Or(q2,w)) -> cnff (Or(q1,cnff (Or(q2,w)) ))
-				| Or(q1,q2) -> p
+				| Or(And(q1,w),q2) -> cnff (And(Or(q2,w),Or(q2,q1)))
+				| Or(q1,And(q2,w)) -> cnff (And(Or(q1,w),Or(q2,q1)))
+				| Or(q1,q2) ->  if (subAnd q1) then cnff (Or ( cnff q1, q2) )
+								else if (subAnd q2) then cnff (Or ( q1, cnff q2) )
+								else p
 				| L(s) -> p
 
 ;;
@@ -100,20 +118,46 @@ let rec dnff p= match p with
 				| F -> F
 				| Not(q) -> p
 				| Or(q1,q2) -> Or(dnff q1, dnff q2)
-				| And(Or(q1,w),q2) ->
-				| And(q1,Or(q2,w)) ->
-				| And(And(q1,w),q2) ->
-				| And(q1,And(q2,w)) ->
-				| And(q1,q2) -> p
+				| And(Or(q1,w),q2) -> dnff ( Or(And(q2,w),And(q2,q1)) )
+				| And(q1,Or(q2,w)) -> dnff ( Or(And(q1,w),And(q2,q1)) )
+				| And(q1,q2) -> if (subOr q1) then dnff (And ( dnff q1, q2) )
+								else if (subOr q2) then dnff (And ( q1, dnff q2) )
+								else p
 				| L(s) -> p
-
 ;;
 
 
 let dnf p = dnff (nnf p);;
 
+let rec isSame p1 p2 = match p1,p2 with
+					| T,T -> true
+					| F,F -> true
+					| Not(q1),Not(q2) -> isSame q1 q2
+					| And(q1,q2),And(q3,q4) -> (isSame q1 q3) && (isSame q2 q4)
+					| Or(q1,q2),Or(q3,q4) -> (isSame q1 q3) && (isSame q2 q4)
+					| Impl(q1,q2),Impl(q3,q4) -> (isSame q1 q3) && (isSame q2 q4)
+					| Iff(q1,q2),Iff(q3,q4) -> (isSame q1 q3) && (isSame q2 q4)
+					| L(s1),L(s2) ->  (s1=s2)
+					| _-> false
+;;
 
-let rec subprop_at p1 p2 = ();;
+(* p1 is subpropostion *)
+
+(* when there is only one path then bit is 0(Not(p)) *)
+
+let rec subprop p1 p2 s str =	if (isSame p1 p2) then SS.add str s
+								else match p2 with
+								| T           -> s
+								| F           -> s
+								| Not(q)      -> subprop p1 q s (str^"0")
+								| And(q1,q2)  -> SS.union (SS.union (subprop p1 q1 s (str^"0") ) (subprop p1 q2 s (str^"1"))) s
+								| Or(q1,q2)   -> SS.union (SS.union (subprop p1 q1 s (str^"0") ) (subprop p1 q2 s (str^"1"))) s
+								| Impl(q1,q2) -> SS.union (SS.union (subprop p1 q1 s (str^"0") ) (subprop p1 q2 s (str^"1"))) s
+								| Iff(q1,q2)  -> SS.union (SS.union (subprop p1 q1 s (str^"0") ) (subprop p1 q2 s (str^"1"))) s
+								| L(st)       -> s
+
+;;
+let subprop_at p1 p2 = subprop p1 p2 SS.empty "";;
 
 (* TestCases *)
 
@@ -122,3 +166,19 @@ print_string ("\n");;
 
 let s = letters (Or(And(L("A"),T),Impl(L("B"),L("A")))) SS.empty;;
 print_set s;;
+
+print_set (subprop_at (Or(And(L("A"),T),Impl(L("B"),L("A")))) (Or(And(L("A"),T),Impl(L("B"),L("A")))))
+;;
+print_set (subprop_at (Impl(L("B"),L("A"))) (Or(And(L("A"),Impl(L("B"),L("A"))),Impl(L("B"),L("A")))))
+;;
+
+
+let a = cnf (Or(And(L("A"),T),Impl(L("B"),L("A"))));;
+let b = dnf (Or(And(L("A"),T),Impl(L("B"),L("A"))));;
+let c = nnf (Or(And(L("A"),T),Impl(L("B"),L("A"))));;
+
+truth a rho;;
+truth b rho;;
+truth c rho;;
+truth (Or(And(L("A"),T),Impl(L("B"),L("A")))) rho;;
+
