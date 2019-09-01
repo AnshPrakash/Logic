9794aac7e41cf68e0304ba21736dd9fc15b034d3
AnshPrakash <anshprakash1997@gmail.com>
2019-09-01 21:00:01 +0530

    find assignment to be done

--- Assignments/A1/A1.ml
+++ Assignments/A1/A1.ml
@@ -1,3 +1,5 @@
+module SS = Set.Make(String);;
+
 
 type prop = T | F | L of string | Not of prop | And of prop*prop 
 			| Or  of prop*prop | Impl of prop*prop | Iff of prop*prop
@@ -56,7 +58,7 @@ let setbase n = match n with
 									else false
 ;;
 
-let rec contrad_path t rho = match t with 
+(* let rec contrad_path t rho = match t with 
 							| Leaf(n) ->(	try (let _ = update n rho in (setbase n))
 											with Contrad -> true
 										)
@@ -68,55 +70,44 @@ let rec contrad_path t rho = match t with
 												) 
 
 ;;
+ *)
 
+let rec contrad_path t rho = match t with 
+							| Leaf(n) ->(	try (let _ = update n rho in ((setbase n);t))
+											with Contrad -> t
+										)
+							| Alpha(n,t1) ->(	try (let rho1 = update n rho in Alpha(n,contrad_path t1 rho1))
+												with Contrad -> t
+											) 
+							| Beta(n,t1,t2)	->	(	try (let rho1 = update n rho in Beta(n,contrad_path t1 rho1,contrad_path t2 rho1) )
+													with Contrad -> t
+												) 
 
+;;
+
+(* 
 let rec select_node t = match t with
 						| Leaf(Node(p,b,s)) -> if (s.value = 0) then (s.value <- 2; true) else false
 						| Alpha(Node(p,b,s),t1) ->  if (s.value = 0) then (s.value <- 2; true) else select_node t1
 						| Beta(Node(p,b,s),t1,t2) ->	if (s.value = 0) then (s.value <- 2; true) 
 														else if (select_node t1) then true 
 														else (select_node t2)
+;; *)
+
+let rec select_node t = match t with
+						| Leaf(Node(p,b,s)) -> if (s.value = 0) then (s.value <- 2; (true,t)) else (false,t)
+						| Alpha(Node(p,b,s),t1) ->  if (s.value = 0) then (s.value <- 2; (true,t)) 
+													else let (tb,tt1) = (select_node t1) in
+															(tb,Alpha(Node(p,b,s),tt1))
+																
+						| Beta(Node(p,b,s),t1,t2) ->	if (s.value = 0) then (s.value <- 2; (true,t) )
+														else let (tb1,tt1) = select_node t1 in
+																if tb1 then (true,Beta(Node(p,b,s),tt1,t2))
+																else let (tb2,tt2) = select_node t2 in
+																		(tb2,Beta(Node(p,b,s),t1,tt2))
+														
 ;;
 
-
-let expand n = match n with
-				|Node(p,b,s) -> (	s.value <- 1;
-									match p with
-									| T -> Leaf(n)
-									| F -> Leaf(n)
-									| Not(q) -> Leaf(Node(q,not b,s))
-									| And(q1,q2) -> if b then Alpha(
-																	Node(p,b,s),
-																	Alpha( Node(q1,true,{value = 0}),
-																		Leaf(Node(q2,true,{value = 0})))
-																		)
-													else Beta(Node(p,b,s),Leaf(Node(q1,false,{value = 0})),Leaf(Node(q2,false,{value = 0})))
-									| Or(q1,q2) ->	if not b then Alpha(Node(p,b,s),
-																	Alpha(
-																		Node(q1,false,{value = 0}),
-																		Leaf(Node(q2,false,{value = 0}))
-																	))
-													else Beta(Node(p,b,s),Leaf(Node(q1,true,{value = 0})),Leaf(Node(q2,true,{value = 0})))
-									| Impl(q1,q2)->	if not b then Alpha(Node(p,b,s),
-																	Alpha(
-																		Node(q1,true,{value = 0}),
-																		Leaf(Node(q2,false,{value = 0}))
-																	)
-																)
-													else Beta(Node(p,b,s),Leaf(Node(q1,false,{value = 0})),Leaf(Node(q2,true,{value = 0})))
-									| Iff(q1,q2) -> if b then Beta(Node(p,b,s),Alpha(Node(q1,true,{value =0})
-																	,Leaf(Node(q2,true,{value =0})))
-																	, Alpha(
-																		
-																			Node(q1,false,{value =0}),Leaf(Node(q2,false,{value =0})))
-																		)
-													else Beta(Node(p,b,s),Alpha(Node(q1,true,{value =0}),
-																				Leaf(Node(q2,false,{value =0})))
-																	, Alpha(Node(q1,false,{value =0}),Leaf(Node(q2,true,{value =0})))
-																)
-									| L(st) -> Leaf(n)
-								)
-;;
 
 let expand t = match t with
 	| Leaf(Node(p,b,s)) -> 	( s.value <- 1;
@@ -194,12 +185,55 @@ let rec step_develop t = match t with
 														else Beta(Node(p,b,s),step_develop t1,step_develop t2)
 ;;
 
+
+let rec full_develop t = 	
+	let (t0) = contrad_path t (Hashtbl.create 10) in 
+	let (b1,t1) = select_node t0 in
+	let (t2) = step_develop t1 in 
+	if b1 then full_develop t2 else t2	
+;;
+
+
+(* let find_assignments t = let t0 = full_develop t in
+	let rec fa rho
+ *)
 (* let rec check_tautology t = *)
 
 (* let rec check_contradiction t =  *)
 
 
+(* Select Node *)
+select_node (Leaf(Node(Or(L("p"),L("q")),true,{value=0})));;
+select_node (Leaf(Node(And(L("p"),L("q")),true,{value=0})));;
+select_node (Leaf(Node(Or(T,F),true,{value=2})));;
 
+
+(* Testing Step develop *)
+step_develop (Leaf(Node(Or(L("p"),L("q")),true,{value=2})));;
+step_develop (Leaf(Node(And(L("p"),L("q")),true,{value=2})));;
+step_develop (Leaf(Node(Or(T,F),true,{value=2})));;
+
+
+(* Testing Contrad Path *)
 contrad_path (Leaf(Node(T,true,{value = 0}))) (Hashtbl.create 1234);;
+contrad_path (step_develop (Leaf(Node(Or(T,F),true,{value=0})))) (Hashtbl.create 12);;
+contrad_path (step_develop (Leaf(Node(Or(T,F),true,{value=2})))) (Hashtbl.create 12);;
+contrad_path (step_develop (Leaf(Node(And(L("p"),L("q")),true,{value=2})))) (Hashtbl.create 12);;
+
+(* full develop *)
+full_develop (Leaf(Node(Or(L("p"),L("q")),true,{value=0})));;
+full_develop (Leaf(Node(And(L("p"),L("q")),true,{value=0})));;
+full_develop (Leaf(Node(Or(T,F),true,{value=2})));; 
 
 
+let t1 = (Or(And(L("A"),T),Impl(L("B"),L("A"))));;
+let t2 = Iff(Not(Or(L("A"),L("B"))),And(Not(L("A")),Not(L("B"))));;
+let t3 =	And(And (Or (Or (L "A", L "B"), Not (L "B")),Or (Not (L "A"), Or (L "A", L "B"))),
+			And (Or (Or (L "A", L "B"), Not (L "B")), Or (Not (L "A"), Or (L "A", L "B"))));;
+
+full_develop (Leaf(Node(t1,true,{value =0})));;
+full_develop (Leaf(Node(t2,true,{value =0})));;
+full_develop (Leaf(Node(t3,true,{value =0})));;
+full_develop (Leaf(Node(t1,false,{value =0})));;
+full_develop (Leaf(Node(t2,false,{value =0})));;
+full_develop (Leaf(Node(t3,false,{value =0})));;
