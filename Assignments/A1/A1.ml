ec5eca413314e4f7d9fcad75dde1daf6e998b2e9
AnshPrakash <anshprakash1997@gmail.com>
2019-09-01 23:54:27 +0530

    Stil some bugs

--- Assignments/A1/A1.ml
+++ Assignments/A1/A1.ml
@@ -32,7 +32,7 @@ type node = Node of prop*bool*status
 type tree = Leaf of node| Alpha of node*tree | Beta of node*tree*tree
 ;;
 
-type example = L of ((string*bool) list)
+type example = LL of ((string*bool) list)
 ;;
 
 type proof = P of tree | E of example
@@ -71,15 +71,16 @@ let setbase n = match n with
 
 ;;
  *)
+(* let rec got_path t = ;; *)
 
 let rec contrad_path t rho = match t with 
-							| Leaf(n) ->(	try (let _ = update n rho in ((setbase n);t))
+							| Leaf(n) ->(	try (let _ = update n rho in (if (setbase n)then t else t))
 											with Contrad -> t
 										)
-							| Alpha(n,t1) ->(	try (let rho1 = update n rho in Alpha(n,contrad_path t1 rho1))
-												with Contrad -> t
+							| Alpha(n,t1) ->(	try (let rho1 = update n rho in (if (setbase n) then t else Alpha(n,contrad_path t1 rho1)) )
+												with Contrad -> t 
 											) 
-							| Beta(n,t1,t2)	->	(	try (let rho1 = update n rho in Beta(n,contrad_path t1 rho1,contrad_path t2 rho1) )
+							| Beta(n,t1,t2)	->	(	try (let rho1 = update n rho in (if (setbase n) then t else Beta(n,contrad_path t1 rho1,contrad_path t2 rho1)) )
 													with Contrad -> t
 												) 
 
@@ -97,14 +98,17 @@ let rec select_node t = match t with
 let rec select_node t = match t with
 						| Leaf(Node(p,b,s)) -> if (s.value = 0) then (s.value <- 2; (true,t)) else (false,t)
 						| Alpha(Node(p,b,s),t1) ->  if (s.value = 0) then (s.value <- 2; (true,t)) 
-													else let (tb,tt1) = (select_node t1) in
+													else if (s.value != -1 ) then 
+															let (tb,tt1) = (select_node t1) in
 															(tb,Alpha(Node(p,b,s),tt1))
-																
+													else (false,t)		
 						| Beta(Node(p,b,s),t1,t2) ->	if (s.value = 0) then (s.value <- 2; (true,t) )
-														else let (tb1,tt1) = select_node t1 in
+														else if (s.value != -1) then
+																let (tb1,tt1) = select_node t1 in
 																if tb1 then (true,Beta(Node(p,b,s),tt1,t2))
 																else let (tb2,tt2) = select_node t2 in
 																		(tb2,Beta(Node(p,b,s),t1,tt2))
+														else (false,t)
 														
 ;;
 
@@ -194,12 +198,53 @@ let rec full_develop t =
 ;;
 
 
-(* let find_assignments t = let t0 = full_develop t in
-	let rec fa rho
- *)
-(* let rec check_tautology t = *)
+let rec unique l = match l with
+				| [] -> []
+				| hd::tl -> if List.mem hd tl then unique tl else hd::(unique tl)
+;;
 
-(* let rec check_contradiction t =  *)
+let mycmp t1 t2 = let (s1,b1) = t1 in 
+	let (s2,b2) = t2 in
+	  if String.length s1 <> String.length s2 then
+	    compare (String.length s2) (String.length s1)
+	  else
+	    String.compare (String.lowercase_ascii s1) (String.lowercase_ascii s2)
+;;
+
+let find_assignments t = let t0 = full_develop t in
+	let rec fa tab rho = match tab with
+					| Leaf(Node(p,b,s)) ->	if (s.value = -1) then []
+											else (	match p with
+														| L(st) -> [(st,b)::rho]
+														| _ -> [rho]
+													)
+					| Alpha(Node(p,b,s),t1) ->	if (s.value = -1) then [] 
+												else (	match p with
+														| L(st) -> fa t1 ((st,b)::rho)
+														| _ -> fa t1 rho
+													)
+					| Beta(Node(p,b,s),t1,t2)->	if (s.value = -1) then [] 
+												else(	match p with
+														| L(st) -> (fa t1 ((st,b)::rho))@(fa t2 ((st,b)::rho))
+														| _ -> (fa t1 rho)@(fa t2 rho)
+													)
+	in let rhos = fa t0 [] in 
+	unique (List.map unique (List.map (List.sort mycmp) rhos))
+;;
+
+(* p :propositon *)
+let rec check_tautology p = let l = find_assignments (Leaf(Node(p,false,{value=0}))) in 
+			match l with
+			| [] -> P(full_develop (Leaf(Node(p,false,{value=0}))))
+			| hd::tl-> E(LL(hd))
+;;
+
+let rec check_contradiction p = let l = find_assignments (Leaf(Node(p,true,{value=0}))) in 
+			match l with
+			| [] -> P(full_develop (Leaf(Node(p,false,{value=0}))))
+			| hd::tl-> E(LL(hd))
+;;
+
 
 
 (* Select Node *)
@@ -237,3 +282,15 @@ full_develop (Leaf(Node(t3,true,{value =0})));;
 full_develop (Leaf(Node(t1,false,{value =0})));;
 full_develop (Leaf(Node(t2,false,{value =0})));;
 full_develop (Leaf(Node(t3,false,{value =0})));;
+
+check_tautology t1 ;;
+check_tautology t2 ;;
+check_tautology t3 ;;
+
+
+let t6 = T;;
+let t7 = Or(L("p"),Not(L("p")));;
+check_tautology t6 ;;
+check_tautology t7;;
+
+check_contradiction (And(t7,Not(t7)));;
